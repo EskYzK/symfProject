b87feadbc6fcc432f257ab5bd37671b2c63e05f4
alexkerber54190 <alexkerber54190@gmail.com>
2025-12-14 00:22:00 +0000

    :sparkles: Mise en place de la modification de ses propres informations de compte

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
+use App\Form\ProfileType;
 use App\Repository\OrderRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/account')]
 #[IsGranted('ROLE_USER')]
@@ -24,6 +27,26 @@ class AccountController extends AbstractController
 
         return $this->render('account/index.html.twig', [
             'orders' => $orders,
+        ]);
+    }
+
+    #[Route('/edit', name: 'app_account_edit')]
+    public function edit(Request $request, EntityManagerInterface $em): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(ProfileType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            $this->addFlash('success', 'Profil mis à jour avec succès !');
+            return $this->redirectToRoute('app_account');
+        }
+
+        return $this->render('account/edit.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
